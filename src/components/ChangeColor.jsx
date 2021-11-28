621b42713189ce778dc37fc3ad77cae4232de248
adham618 <adhamtarek291@gmail.com>
2021-11-28 19:42:07 +0200

    create Changecolor component

---
+++ src/components/ChangeColor.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { changeColor } from "../features/theme";
+
+const ChangeColor = () => {
+  const [color, setColor] = useState("");
+  const dispatch = useDispatch();
+  return (
+    <div className="change-color">
+      <input type="text" onChange={(event) => setColor(event.target.value)} />
+      <button onClick={() => { dispatch(changeColor(color)) }}>change color</button>
+    </div>
+  );
+}
+
+export default ChangeColor;
