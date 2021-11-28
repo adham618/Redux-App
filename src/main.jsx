9071dffcf39e402844d47b0b7da961e41ba4f209
adham618 <adhamtarek291@gmail.com>
2021-11-28 17:32:07 +0200

    creating a  redux store

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
+import { configureStore } from '@reduxjs/toolkit'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import App from './App'
+const store = configureStore({
+  reducer: {}
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
